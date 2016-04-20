fe2f86e66c270e463860923a5da0b71638e562d7
GhostGod <745089707@qq.com>
2016-04-20 16:29:13 +0800

    wwww

--- Test/src/main/java/com/demo/Test/App.java
+++ Test/src/main/java/com/demo/Test/App.java
@@ -4,10 +4,10 @@ package com.demo.Test;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );法国红酒对方好感
+        
     }
 }
