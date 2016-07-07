d2693b749f486a20e22bbaefb2d262ae049a0913
GhostGod <745089707@qq.com>
2016-07-07 10:26:46 +0800

    测试

--- Test/src/main/java/com/demo/Test/App.java
+++ Test/src/main/java/com/demo/Test/App.java
@@ -1,5 +1,7 @@
 package com.demo.Test;
 
+import java.util.TreeMap;
+
 /**
  * Hello world!
  *
@@ -7,6 +9,6 @@ package com.demo.Test;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		TreeMap<K, V>
 	}
 }
